8424db16bfd81fff7edb40cdd0535eef85610544
peilerx <peiler.dany@gmail.com>
2025-05-23 15:33:44 +0300

    greetings

--- l_1/src/main.rs
+++ l_1/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    greet_world();
+}
+
+fn greet_world() {
+    let english = "Hello World!";
+    let german = "Grüß Gott!";
+    let japanese = "こんにちは、世界よ！";
+    let regions = [german, japanese, english];
+    for region in regions.iter() {
+        println!("{}", region);
+    }
+
+    println!("");
+
+    for region in regions.iter().rev() {
+        println!("{}", region);
+    }
 }
